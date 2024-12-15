8df91c906eecf81be4b0c7c18ace99180e6de451
arturino009 <arturino009@gmail.com>
2024-12-15 23:48:47 +0200

    first commit

---
+++ AtlasScanner.cs
@@ -0,0 +1,92 @@
+﻿using System.Drawing;
+using System.Numerics;
+using ExileCore2;
+using ExileCore2.Shared.Enums;
+using Microsoft.Win32;
+
+namespace AtlasScanner
+{
+    public class AtlasScanner : BaseSettingsPlugin<AtlasScannerSettings>
+    {
+        public override bool Initialise()
+        {
+            return true;
+        }
+
+
+        public override void Render()
+        {
+            var atlasPanel = GameController.Game.IngameState.IngameUi.WorldMap.AtlasPanel;
+            int uniqueMaps = 0;
+            int otherMaps = 0;
+            int traderMaps = 0;
+            if (atlasPanel.IsVisible)
+            {
+                var maps = atlasPanel.Descriptions;
+                foreach (var tile in maps)
+                {
+                    var mapElement = tile.Element;
+                    var mapName = mapElement.Area.Name;
+                    var currentlyAvailable = mapElement.IsUnlocked;
+                    var completed = mapElement.IsVisited;
+                    var leagueMechanic = mapElement.Children[0].Children;
+                    if (leagueMechanic.Count > 0)
+                    {
+                        foreach (var mechanic in leagueMechanic)
+                        {
+                            var texture = mechanic.TextureName;
+                            var finalColor = Color.Transparent;
+                            switch (texture)
+                            {
+                                case var _ when texture.Contains("AtlasIconContentMapBoss"):
+                                    if (Settings.ShowBoss) finalColor = Settings.BossColor;
+                                    break;
+                                case var _ when texture.Contains("AtlasIconContentBreach"):
+                                    if (Settings.ShowBreach) finalColor = Settings.BreachColor;
+                                    break;
+                                case var _ when texture.Contains("AtlasIconContentExpedition"):
+                                    if (Settings.ShowExpedition) finalColor = Settings.ExpeditionColor;
+                                    break;
+                                case var _ when texture.Contains("AtlasIconContentRitual"):
+                                    if (Settings.ShowRitual) finalColor = Settings.RitualColor;
+                                    break;
+                                case var _ when texture.Contains("AtlasIconContentDelirium"):
+                                    if (Settings.ShowDelirium) finalColor = Settings.DeliriumColor;
+                                    break;
+                                case var _ when texture.Contains("AtlasIconContentCorruption"):
+                                    if (Settings.ShowCorruption) finalColor = Settings.CorruptionColor;
+                                    break;
+                                case var _ when texture.Contains("AtlasIconContentIrradiated"):
+                                    if (Settings.ShowIrradiated) finalColor = Settings.IrradiatedColor;
+                                    break;
+                                case var _ when texture.Contains("AtlasIconContentUniqueMap"):
+                                    if (Settings.ShowUnique) finalColor = Settings.UniqueColor;
+                                    uniqueMaps++;
+                                    break;
+                                case var _ when texture.Contains("AtlasIconContentHideout"):
+                                    if (Settings.ShowHideout) finalColor = Settings.HideoutColor;
+                                    break;
+                                case var _ when texture.Contains("AtlasIconContentTrader"):
+                                    finalColor = Settings.HideoutColor;
+                                    traderMaps++;
+                                    break;
+                                default:
+                                    if (Settings.ShowOther) finalColor = Settings.OtherColor;
+                                    otherMaps++;
+                                    break;
+                            }
+                            if (finalColor != Color.Transparent) Graphics.DrawFrame(mechanic.GetClientRect(), finalColor, Settings.FrameThickness.Value);
+                        }
+                    }
+                    if (!completed && Settings.MapNames) Graphics.DrawTextWithBackground(mapName, mapElement.GetClientRect().Center, Color.White, FontAlign.Center, Color.Black);
+                }
+                var rect = new Vector2(10, 50);
+                var rect1 = new Vector2(10, 70);
+                var rect2 = new Vector2(10, 90);
+                //Graphics.DrawTextWithBackground("Found " + uniqueMaps + " unique maps", rect, Color.Green, Color.Black);
+                Graphics.DrawTextWithBackground("Found " + traderMaps + " trader maps", rect1, Color.Green, Color.Black);
+                //Graphics.DrawTextWithBackground("Found " + otherMaps + " other maps", rect2, Color.Green, Color.Black);
+            }
+        }
+    }
+}
---
+++ AtlasScannerSettings.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing;
+using ExileCore2.Shared.Attributes;
+using ExileCore2.Shared.Interfaces;
+using ExileCore2.Shared.Nodes;
+
+namespace AtlasScanner
+{
+    public class AtlasScannerSettings : ISettings
+    {
+        public ToggleNode Enable { get; set; } = new ToggleNode(false);
+
+        public RangeNode<int> FrameThickness { get; set; } = new RangeNode<int>(2, 1, 20);
+
+        [Menu("Highlight Boss")]
+        public ToggleNode ShowBoss { get; set; } = new ToggleNode(false);
+        [Menu("Highlight Breach")]
+        public ToggleNode ShowBreach { get; set; } = new ToggleNode(false);
+        [Menu("Highlight Expedition")]
+        public ToggleNode ShowExpedition { get; set; } = new ToggleNode(false);
+        [Menu("Highlight Ritual")]
+        public ToggleNode ShowRitual { get; set; } = new ToggleNode(false);
+        [Menu("Highlight Delirium")]
+        public ToggleNode ShowDelirium { get; set; } = new ToggleNode(false);
+        [Menu("Highlight Corruption")]
+        public ToggleNode ShowCorruption { get; set; } = new ToggleNode(false);
+        [Menu("Highlight Irradiated")]
+        public ToggleNode ShowIrradiated { get; set; } = new ToggleNode(false);
+        [Menu("Highlight Unique Maps")]
+        public ToggleNode ShowUnique { get; set; } = new ToggleNode(false);
+        [Menu("Highlight Hideouts")]
+        public ToggleNode ShowHideout { get; set; } = new ToggleNode(false);
+        [Menu("Highlight Other (?) Maps")]
+        public ToggleNode ShowOther { get; set; } = new ToggleNode(false);
+
+        [Menu("Boss Color")]
+        public ColorNode BossColor { get; set; } = new ColorNode(Color.White);
+        [Menu("Breach Color")]
+        public ColorNode BreachColor { get; set; } = new ColorNode(Color.Purple);
+        [Menu("Expedition Color")]
+        public ColorNode ExpeditionColor { get; set; } = new ColorNode(Color.Gray);
+        [Menu("Ritual Color")]
+        public ColorNode RitualColor { get; set; } = new ColorNode(Color.Red);
+        [Menu("Delirium Color")]
+        public ColorNode DeliriumColor { get; set; } = new ColorNode(Color.DarkGray);
+        [Menu("Corruption Color")]
+        public ColorNode CorruptionColor { get; set; } = new ColorNode(Color.DarkRed);
+        [Menu("Irradiated Color")]
+        public ColorNode IrradiatedColor { get; set; } = new ColorNode(Color.Green);
+        [Menu("Unique Color")]
+        public ColorNode UniqueColor { get; set; } = new ColorNode(Color.Brown);
+        [Menu("Hideout Color")]
+        public ColorNode HideoutColor { get; set; } = new ColorNode(Color.LightBlue);
+        [Menu("Other Color")]
+        public ColorNode OtherColor { get; set; } = new ColorNode(Color.Pink);
+
+        public EmptyNode Space { get; set; } = new EmptyNode();
+        [Menu("Draw map names")]
+        public ToggleNode MapNames { get; set; } = new ToggleNode(false);
+    }
+}
